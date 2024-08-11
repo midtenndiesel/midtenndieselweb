7db9115d7e87abb019addc3aeca5df6c377684c4
Seth Towe <52022604+stowe1@users.noreply.github.com>
2024-08-11 16:23:06 -0500

    commit

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -34,128 +34,47 @@ const Portfolio = () => {
   return (
     <div
       id="contact"
-      className="flex justify-center items-center mt-8 w-full bg-white py-12 lg:py-24 "
+      className="flex justify-center items-center mt-8 w-full bg-white py-12 lg:py-24 max-w-[35%] mx-auto"
     >
-      <div className="container mx-auto my-8 px-4 lg:px-20" data-aos="zoom-in">
-        <form>
-          <div className="w-full bg-white p-8 my-4 md:px-12 lg:w-9/12 lg:pl-20 lg:pr-40 mr-auto rounded-2xl shadow-2xl">
-            <div className="flex">
-              <h1 className="font-bold text-center lg:text-left text-blue-900 uppercase text-4xl">
-                Send us a message
-              </h1>
-            </div>
-            <div className="grid grid-cols-1 gap-5 md:grid-cols-2 mt-5">
-              <div>
-                <input
-                  name="first_name"
-                  className="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
-                  type="text"
-                  placeholder="First Name*"
-                  value={firstName}
-                  onChange={(e) => setFirstName(e.target.value)}
-                  onKeyUp={clearErrors}
-                />
-                {errors && (
-                  <p className="text-red-500 text-sm">{errors.first_name}</p>
-                )}
-              </div>
-
-              <div>
-                <input
-                  name="last_name"
-                  className="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
-                  type="text"
-                  placeholder="Last Name*"
-                  value={lastName}
-                  onChange={(e) => setLastName(e.target.value)}
-                  onKeyUp={clearErrors}
-                />
-                {errors && (
-                  <p className="text-red-500 text-sm">{errors.last_name}</p>
-                )}
-              </div>
-
-              <div>
-                <input
-                  name="email"
-                  className="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
-                  type="email"
-                  placeholder="Email*"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  onKeyUp={clearErrors}
-                />
-                {errors && (
-                  <p className="text-red-500 text-sm">{errors.email}</p>
-                )}
-              </div>
-
-              <div>
-                <input
-                  name="phone_number"
-                  className="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
-                  type="number"
-                  placeholder="Phone*"
-                  value={phone}
-                  onChange={(e) => setPhone(e.target.value)}
-                  onKeyUp={clearErrors}
-                />
-                {errors && (
-                  <p className="text-red-500 text-sm">{errors.phone_number}</p>
-                )}
-              </div>
-            </div>
-            <div className="my-4">
-              <textarea
-                name="message"
-                placeholder="Message*"
-                className="w-full h-32 bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
-                value={message}
-                onChange={(e) => setMessage(e.target.value)}
-                onKeyUp={clearErrors}
-              ></textarea>
-              {errors && (
-                <p className="text-red-500 text-sm">{errors.message}</p>
-              )}
-            </div>
-            <div className="my-2 w-1/2 lg:w-2/4">
-              <button
-                type="submit"
-                id="submitBtn"
-                className="uppercase text-sm font-bold tracking-wide bg-gray-500 hover:bg-blue-900 text-gray-100 p-3 rounded-lg w-full 
-                        focus:outline-none focus:shadow-outline"
-              >
-                Send Message
-              </button>
-            </div>
+      <div
+        className="container mx-auto my-8 lg:px-20 min-h-max"
+        data-aos="zoom-in"
+      >
+        <div className="w-full bg-white p-8 my-4 mx-auto rounded-2xl shadow-2xl">
+          <div className="flex justify-center items-center">
+            <h1 className="font-bold text-center lg:text-left text-blue-900 uppercase text-4xl">
+              Contact Us
+            </h1>
           </div>
-        </form>
-        <div className="w-full  lg:-mt-96 lg:w-2/6 px-8 py-6 ml-auto bg-blue-900 rounded-2xl">
-          <div className="flex flex-col text-white">
-            <div className="flex my-4 w-2/3 lg:w-3/4">
-              <div className="flex flex-col">
-                <i className="fas fa-map-marker-alt pt-2 pr-2" />
-              </div>
-              <div className="flex flex-col">
-                <h2 className="text-2xl">Office Address</h2>
-                <p className="text-gray-400">
-                  2205 Center Point Rd, Hendersonville, TN 37075
-                </p>
-              </div>
-            </div>
 
-            <div className="flex my-4 w-2/3 lg:w-1/2">
-              <div className="flex flex-col">
-                <i className="fas fa-phone-alt pt-2 pr-2" />
+          {/* contact */}
+          <div className="bg-blue-900 rounded-2xl m-12">
+            <div className="flex flex-col text-white">
+              <div className="flex justify-center items-center">
+                <div className="flex flex-col">
+                  <i className="fas fa-map-marker-alt pt-2" />
+                </div>
+                <div className="flex flex-col pt-6">
+                  <h2 className="text-2xl text-center">Office Address</h2>
+                  <p className="text-gray-400 text-center">
+                    2205 Center Point Rd, Dunlap, TN 37075
+                  </p>
+                </div>
               </div>
 
-              <div className="flex flex-col">
-                <h2 className="text-2xl">Call Us</h2>
-                <p className="text-gray-400">Tel: (615) 925-0118</p>
+              <div className="flex justify-center items-center  pt-6">
+                <div className="flex flex-col">
+                  <h2 className="text-2xl text-center">Call Us</h2>
+                  <p className="text-gray-400 text-center">
+                    Tel: (615) 925-0118
+                  </p>
 
-                <div className="mt-5">
-                  <h2 className="text-2xl">Send an E-mail</h2>
-                  <p className="text-gray-400"> middtenndiesel@aol.com</p>
+                  <div className="mt-5 pb-6">
+                    <h2 className="text-2xl text-center">Send an E-mail</h2>
+                    <p className="text-gray-400 text-center">
+                      middtenndiesel@aol.com
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
